ad4ba28e44086931536918f5c0b138e2280f199f
Ives van Hoorne <Ives.v.h@gmail.com>
2015-06-16 16:47:36 +0200

    visual changes, added non-working input bar (strange route error)

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  get 'chat/index'
-  get 'chat/send'
-
   root 'chat#index'
+
+  resource :chat, only: [:index, :create]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
