7fc5a1cf379574064a8fcf1ea89d581ab00afddb
Ives van Hoorne <Ives.v.h@gmail.com>
2015-06-16 17:30:08 +0200

    Message can now be saved

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -4,5 +4,10 @@ class ChatController < ApplicationController
   end
 
   def create
+    @message = Message.new(message: params[:message])
+    @message.save
+    respond_to do |format|
+      format.html { Rails.logger.debug "Hai".inspect }
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'chat#index'
 
-  resource :chat, only: [:index, :create]
+  resources :chat, only: [:index, :create]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
