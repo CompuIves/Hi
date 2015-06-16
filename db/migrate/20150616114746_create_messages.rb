9e9fbb6f856b71a071ba1efdbb1ef3aaf34e2347
Ives van Hoorne <Ives.v.h@gmail.com>
2015-06-16 22:28:33 +0200

    fixed malicious migration

--- db/migrate/20150616114746_create_messages.rb
+++ db/migrate/20150616114746_create_messages.rb
@@ -2,7 +2,6 @@ class CreateMessages < ActiveRecord::Migration
   def change
     create_table :messages do |t|
       t.string :message
-      t.belongs_to :user, index: true, foreign_key: true
 
       t.timestamps null: false
     end
