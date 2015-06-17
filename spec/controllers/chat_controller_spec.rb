59fa3df95ad81d4d3d33cd5706104febf9f490e4
Ives van Hoorne <Ives.v.h@gmail.com>
2015-06-17 09:23:11 +0200

    fix spec

--- spec/controllers/chat_controller_spec.rb
+++ spec/controllers/chat_controller_spec.rb
@@ -1,8 +1,11 @@
 require 'rails_helper'
 
 describe ChatController do
+  let(:message) { Fabricate :message, message: 'some_chat_message' }
+
   describe '#index' do
     it 'works' do
+      expect(message).to be_persisted
       get :index
       expect(response).to be_ok
     end
